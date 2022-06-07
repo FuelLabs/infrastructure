00207ef49d38dba09aa2926cc74aa9788eb55fcb
roy_fuel <96140895+rfuelsh@users.noreply.github.com>
2022-06-06 23:01:58 -0400

    deploy k8s logging (#31)
    * k8s logging deployment
    
    * removing newline
    
    * Adding kibana ingress deploy script
    
    * fixing newline issue

--- scripts/create-k8s.sh
+++ scripts/create-k8s.sh
@@ -27,15 +27,21 @@ sleep 600
 echo "Now setting up your k8s cluster ..."
 
 if [ "${k8s_provider}" == "eks" ]; then
+    cd ../../../ingress/
     echo "Updating your kube context locally ....."
     aws eks update-kubeconfig --name ${TF_VAR_eks_cluster_name}
-    cd ../ingress/${k8s_provider}
     echo "Deploying cert-manager helm chart to ${TF_VAR_eks_cluster_name} ...."
     helm repo add jetstack https://charts.jetstack.io
     helm repo update
     helm upgrade cert-manager jetstack/cert-manager --namespace cert-manager --version v1.7.1 --install --create-namespace
+    echo "Deploying production cluster issuer to ${TF_VAR_eks_cluster_name} ...."
+    mv prod-issuer.yaml prod-issuer.template
+    envsubst < prod-issuer.template > prod-issuer.yaml
+    rm prod-issuer.template
+    kubectl apply -f prod-issuer.yaml
     echo "Deploying nginx ingress controller to ${TF_VAR_eks_cluster_name} ...."
     kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/aws/1.21/deploy.yaml 
+    sleep 300 
     echo "Deploying kube-prometheus helm chart to ${TF_VAR_eks_cluster_name} ...."
     helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
     helm repo update
@@ -47,6 +53,12 @@ if [ "${k8s_provider}" == "eks" ]; then
               --timeout 8000s \
               --debug
               --version ^34
+    sleep 300
+    echo "Deploying monitoring ingress to ${TF_VAR_eks_cluster_name} ...."
+    mv monitoring-ingress.yaml monitoring-ingress.template
+    envsubst < monitoring-ingress.template > monitoring-ingress.yaml
+    rm monitoring-ingress.template
+    kubectl apply -f monitoring-ingress.yaml
 else
    echo "You have inputted a non-supported kubernetes provider in your .env"
 fi
