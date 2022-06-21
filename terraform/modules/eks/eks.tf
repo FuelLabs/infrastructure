791fff6c0180755bd100fd627a7ab3593c66cd09
Ryan Sproule <rsproule23@gmail.com>
2022-06-21 13:38:53 -0700

    correct wrong cidr var name

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -194,7 +194,7 @@ resource "aws_security_group" "eks-cluster-sg" {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["${var.vpc_cidr_block}"]
+    cidr_blocks = ["${var.aws_vpc_cidr_block}"]
   }
 
   egress {
@@ -216,7 +216,7 @@ ingress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
-    cidr_blocks = ["${var.vpc_cidr_block}"]
+    cidr_blocks = ["${var.aws_vpc_cidr_block}"]
   }
 
   egress {
