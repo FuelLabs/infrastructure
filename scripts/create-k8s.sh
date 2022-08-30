40f7600bb74acb8f3cf1065be17731d8453fb9da
rfuelsh <yuvraj.singh@fuel.sh>
2022-08-29 22:38:46 -0400

    add updated nginx-ingress url

--- scripts/create-k8s.sh
+++ scripts/create-k8s.sh
@@ -59,7 +59,7 @@ if [ "${k8s_provider}" == "eks" ]; then
     echo "Deploying elasticsearch to ${TF_VAR_eks_cluster_name} ...."
     cd ../logging/elasticsearch
     kubectl create -f https://download.elastic.co/downloads/eck/2.2.0/crds.yaml
-    kubectl apply -f https://download.elastic.co/downloads/eck/2.2.0/operator.yam
+    kubectl apply -f https://download.elastic.co/downloads/eck/2.2.0/operator.yaml
     kubectl create ns logging
     kubectl apply -f logging-cluster.yaml
     sleep 180
