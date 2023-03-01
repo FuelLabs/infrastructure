9e4dcc687369265b7b0c7937a2d2ed8ce32ba08c
David Young <david.young@fuel.sh>
2023-03-01 14:04:00 -0500

    Simplify curl request

--- scripts/create-k8s.sh
+++ scripts/create-k8s.sh
@@ -187,6 +187,7 @@ setup_eks_container() {
 
     local doc=$(curl $cloudwatch_url | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{region_name}}/'${RegionName}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/')
 
+    echo "$doc"
     echo "$doc" | kubectl apply -f -
 
     # curl $cloudwatch_url | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{region_name}}/'${RegionName}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' | kubectl apply -f -
