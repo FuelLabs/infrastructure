0ea730031868b7e56b04d7643d54f2f54d74690d
David Young <david.young@fuel.sh>
2023-03-01 11:41:06 -0500

    Looking at sed expression

--- scripts/create-k8s.sh
+++ scripts/create-k8s.sh
@@ -195,14 +195,18 @@ setup_eks_container() {
 
     [[ -n $cw_data ]] || fail "nothing retrieved from $cloudwatch_url"
 
-    local kubedata=$(sed -e 's/{{cluster_name}}/'${ClusterName}'/' \
-                         -e 's/{{region_name}}/'${RegionName}'/' \
-                         -e 's/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/' \
-                         -e 's/{{http_server_port}}/"'${FluentBitHttpPort}'"/' \
-                         -e 's/{{read_from_head}}/"'${FluentBitReadFromHead}'"/' \
-                         -e 's/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' <<< $cw_data)
+    echo $cw_data
+    
+    echo $cw_data | sed 's/{{cluster_name}}/'${ClusterName}'/;s/{{region_name}}/'${RegionName}'/;s/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/;s/{{http_server_port}}/"'${FluentBitHttpPort}'"/;s/{{read_from_head}}/"'${FluentBitReadFromHead}'"/;s/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' | kubectl apply -f -
 
-    echo $kubedata | kubectl apply -f -
+    # local kubedata=$(sed -e 's/{{cluster_name}}/'${ClusterName}'/' \
+    #                      -e 's/{{region_name}}/'${RegionName}'/' \
+    #                      -e 's/{{http_server_toggle}}/"'${FluentBitHttpServer}'"/' \
+    #                      -e 's/{{http_server_port}}/"'${FluentBitHttpPort}'"/' \
+    #                      -e 's/{{read_from_head}}/"'${FluentBitReadFromHead}'"/' \
+    #                      -e 's/{{read_from_tail}}/"'${FluentBitReadFromTail}'"/' <<< $cw_data)
+
+    # echo $kubedata | kubectl apply -f -
 }
 
 setup_elastic() {
