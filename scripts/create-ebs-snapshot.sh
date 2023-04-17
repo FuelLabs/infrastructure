7ec242cfa8607d5b84e82621ac24c6cb7a43ed5d
roy_fuel <96140895+rfuelsh@users.noreply.github.com>
2023-04-17 04:38:28 -0400

    fix grammar mistakes

--- scripts/create-ebs-snapshot.sh
+++ scripts/create-ebs-snapshot.sh
@@ -22,7 +22,7 @@ aws ec2 create-snapshot --volume-id $ORG_SNAPSHOT_ID
 echo "Enter your SnapshotId from above command output ...."
 read FINAL_SNAPSHOT_ID
 
-echo "Wait until you Progress is 100% before deploying fuel-core ...."
+echo "Wait until progress is 100% before deploying fuel-core ...."
 aws ec2 describe-snapshots --snapshot-ids $FINAL_SNAPSHOT_ID
-echo "Make sure to utilizze $FINAL_SNAPSHOT_ID for fuel_core_pvc_snapshot_ref in fuel-deployment env file"
+echo ""Make sure to utilize $FINAL_SNAPSHOT_ID for fuel_core_pvc_snapshot_ref in fuel-deployment env file"
 
