b38435f6a570b495feed2adfffbd0b3cd9e5ea88
rfuelsh <yuvraj.singh@fuel.sh>
2023-07-11 15:34:44 -0400

    Adding new vpc changes

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -90,6 +90,8 @@ resource "aws_eks_node_group" "nodes" {
     aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
     aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
     aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
+    module.eks
+
   ]
 }
 
