02996b0d5be338812c9c610a390d18bd58f42621
rfuelsh <yuvraj.singh@fuel.sh>
2023-07-12 02:21:26 -0400

    add public endpoint

--- terraform/modules/eks/eks.tf
+++ terraform/modules/eks/eks.tf
@@ -6,7 +6,7 @@ module "eks" {
   cluster_name                    = "${var.eks_cluster_name}"
   cluster_version                 = "${var.eks_cluster_version}"
   cluster_endpoint_private_access = true
-  cluster_endpoint_public_access  = false
+  cluster_endpoint_public_access  = true
 
   vpc_id     = module.vpc.vpc_id
   subnet_ids = module.vpc.private_subnets
